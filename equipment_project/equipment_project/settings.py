d63a94e4f06023fed52f7ed08d8e7dfbd70a05b6
Pavelel Kalinin <mono-gluk@ya.ru>
2022-10-11 15:49:02 +0300

    Remarks with serial number column and create form

--- equipment_project/equipment_project/settings.py
+++ equipment_project/equipment_project/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='root'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT', default='5432'),
